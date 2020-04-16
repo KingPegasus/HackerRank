395c2484d6f8b0c850d64666f1c9a2f0f37d9cb8
Muhammad Raza Khalid Saleemi <rks143@gmail.com>
2020-04-16 22:58:34 +0500

    Delete Find Angle MBC.py

--- Medium/Find Angle MBC.py
+++
@@ -1,6 +0,0 @@
-#https://www.hackerrank.com/challenges/find-angle/problem
-from math import atan , degrees
-AB = int(input())
-BC = int(input())
-Theta = degrees( atan(AB/BC) )
-print( str(int(round(Theta,0))) + "°")
