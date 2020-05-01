7500ca54a654eda254a7b3f944b102f93aaa85b1
Muhammad Raza Khalid Saleemi <rks143@gmail.com>
2020-05-01 20:12:23 +0500

    Add files via upload

---
+++ Python/Medium/Write a function.py
@@ -0,0 +1,23 @@
+#https://www.hackerrank.com/challenges/write-a-function/problem
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if year%4==0:
+        if year>=100:
+            if year%100==0:
+                if year>=400:
+                    if year%400==0:
+                        leap= True;
+                    else:
+                        leap = False;
+                else:
+                    leap= True;
+            else:
+                leap = True;
+        else:
+             leap = True;
+            
+    return leap
+
+
