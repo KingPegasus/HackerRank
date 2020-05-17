277f3137bc910c71486614ffc6f12dd482272ac4
Muhammad Raza Khalid Saleemi <rks143@gmail.com>
2020-05-17 22:46:45 +0500

    Add files via upload

---
+++ Problem Solving/Algorithms/Circular Array Rotation.py
@@ -0,0 +1,41 @@
+#https://www.hackerrank.com/challenges/circular-array-rotation/problem
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the circularArrayRotation function below.
+def circularArrayRotation(a, k, queries):
+    out = []
+    for q in queries:
+        out.append( a[(q-k)%len(a)] )
+    return out
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nkq = input().split()
+
+    n = int(nkq[0])
+
+    k = int(nkq[1])
+
+    q = int(nkq[2])
+
+    a = list(map(int, input().rstrip().split()))
+
+    queries = []
+
+    for _ in range(q):
+        queries_item = int(input())
+        queries.append(queries_item)
+
+    result = circularArrayRotation(a, k, queries)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
